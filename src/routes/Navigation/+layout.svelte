0df6d9f086bff9dec2c3ef3042d3e250b65723b8
Shaun Mulligan <switchflip.shaun@gmail.com>
2022-11-26 14:35:19 +0100

    render map with GPS location marker

---
+++ src/routes/Navigation/+layout.svelte
@@ -0,0 +1,5 @@
+<script>
+    import 'leaflet/dist/leaflet.css' //Need for leaflet map to render correctly
+</script>
+
+<slot />
--- src/routes/Navigation/+page.svelte
+++ src/routes/Navigation/+page.svelte
@@ -1,10 +1,34 @@
 <script>
-    export let data;
-    let state = data.wsStore
+import { LeafletMap, TileLayer, Marker} from 'svelte-leafletjs';
+
+let mapOptions = {
+    center: [41.483561819705194, 2.214374347050403],
+    zoom: 17,
+};
+
+const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+const tileLayerOptions = {
+    minZoom: 0,
+    maxZoom: 20,
+    maxNativeZoom: 19,
+    attribution: "© OpenStreetMap contributors",
+};
+
+export let data;
+let leafletMap;
+let myStore = data.wsStore;
 </script>
 
-<ion-content fullscreen>
-    <ion-label>
-        Navigation page
-    </ion-label>
-</ion-content>
+<div class="map" >
+<LeafletMap bind:this={leafletMap} options={mapOptions = {center: [$myStore.latitude, $myStore.longitude], zoom: 17}} >
+    <TileLayer url={tileUrl} options={tileLayerOptions} />
+    <Marker latLng={[$myStore.latitude, $myStore.longitude]}/>
+</LeafletMap>
+</div>
+
+<style>
+.map{
+    height: 100%;
+width: 100%;
+}
+</style>
