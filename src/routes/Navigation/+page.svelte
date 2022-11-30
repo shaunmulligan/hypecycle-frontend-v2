45b897ea15d0f30907f12dfe721649f53b9277cd
Shaun Mulligan <switchflip.shaun@gmail.com>
2022-11-30 16:53:13 +0100

    uses leaflet.offline to store maps

--- src/routes/Navigation/+page.svelte
+++ src/routes/Navigation/+page.svelte
@@ -1,5 +1,10 @@
 <script>
+import { onMount } from 'svelte';
 import { LeafletMap, TileLayer, Marker, GeoJSON } from 'svelte-leafletjs';
+import 'leaflet.offline';
+import L from 'leaflet';
+import { urlTemplate } from '../../lib/services/offline-map-const';
+import storageLayer from '$lib/services/storageLayer';
 
 const updateTrackInterval = 1000; //Update every second might be heavy on browser perf
 
@@ -42,6 +47,56 @@ function routeReloadTimer() {
             }, updateTrackInterval);
         }
 routeReloadTimer()
+
+onMount(async () => {
+    let map = leafletMap.getMap()
+    const baseLayer = L.tileLayer
+        .offline(urlTemplate, {
+            attribution: 'Map data {attribution.OpenStreetMap}',
+            subdomains: 'abc',
+            minZoom: 11,
+        })
+        .addTo(map)
+    
+    const control = L.control.savetiles(baseLayer, {
+        zoomlevels: [15, 11], // optional zoomlevels to save, default current zoomlevel
+        confirm(layer, successCallback) {
+            // eslint-disable-next-line no-alert
+            if (window.confirm(`Save ${layer._tilesforSave.length}`)) {
+                console.log("saving tiles")
+            successCallback();
+            }
+        },
+        confirmRemoval(layer, successCallback) {
+            // eslint-disable-next-line no-alert
+            if (window.confirm('Remove all the tiles?')) {
+            successCallback();
+            }
+        },
+        saveText:
+            '<i class="fa fa-download" aria-hidden="true" title="Save tiles"></i>',
+        rmText: '<i class="fa fa-trash" aria-hidden="true"  title="Remove tiles"></i>',
+    });
+    control.addTo(map);
+
+    map.setView(
+    {
+        lat: 41.393137,
+        lng: 2.1363948,
+    },
+    15,);
+
+    let progress;
+    baseLayer.on('savestart', (e) => {
+        progress = 0;
+        let total = e._tilesforSave.length;
+        console.log(total)
+    });
+    baseLayer.on('savetileend', () => {
+        progress += 1;     
+        console.log(progress)
+    });
+    });
 </script>
 
 <div class="map" >
