030ea8aef050b3116b7d1701d7254ceae6fd00c6
Shaun Mulligan <switchflip.shaun@gmail.com>
2022-12-02 17:03:24 +0100

    Add analytics page

--- src/routes/Analytics/+page.svelte
+++ src/routes/Analytics/+page.svelte
@@ -1,13 +1,80 @@
 <script>
+    import MetricWidget from "$lib/components/MetricWidget.svelte";
+    import ZoneWidget from "$lib/components/ZoneWidget.svelte";
+    import Timer from "$lib/components/TimerWidget.svelte"
+
     export let data;
     let state = data.wsStore
 </script>
 
 <ion-content fullscreen>
-    <ion-label>
-        Analytics page
-    </ion-label>
-    <ion-text>
-        {JSON.stringify($state)}
-    </ion-text>
+<ion-grid>
+    <ion-row>
+    <ion-col>
+        <div>
+        <MetricWidget label="Avg. Speed" value={$state.avg_speed} unit="km/h" />
+        </div>
+    </ion-col>
+    <ion-col>
+        <div>
+            <MetricWidget label="Max. Speed" value={$state.max_speed} unit="km/h" />
+        </div>
+    </ion-col>
+    </ion-row>
+    <ion-row>
+    <ion-col>
+        <div>
+            <ZoneWidget label="Avg. Heart Rate" value={$state.avg_hr} unit="bpm" zones={[148,177]}/>
+        </div>
+    </ion-col>
+    <ion-col>
+        <div>
+        <ZoneWidget label="Max. Heart Rate" value={$state.max_hr} unit="bpm" zones={[220,301]}/>
+        </div>
+    </ion-col>
+    </ion-row>
+<ion-row>
+    <ion-col>
+        <div>
+        <ZoneWidget label="Avg. Power" value={$state.avg_power} unit="W" zones={[148,177]}/>
+        </div>
+    </ion-col>
+    <ion-col>
+    <div>
+        <ZoneWidget label="Max. Power" value={$state.max_power} unit="W" zones={[220,301]}/>
+    </div>
+    </ion-col>
+</ion-row>
+</ion-grid>
 </ion-content>
+
+<style>
+ion-menu-button {
+    color: var(--ion-color-primary);
+}
+
+#container {
+    text-align: center;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+}
+
+#container strong {
+    font-size: 20px;
+    line-height: 26px;
+}
+
+#container p {
+    font-size: 16px;
+    line-height: 22px;
+    color: #8c8c8c;
+    margin: 0;
+}
+
+#container a {
+    text-decoration: none;
+}
+</style>
